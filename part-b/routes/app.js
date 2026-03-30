0ba6a30a68c66d82e766dcf50020a3f6a2b1e615
sungw <112956363+sungw3@users.noreply.github.com>
2026-03-30 20:56:40 +0900

    Assignment part-b

--- part-b/routes/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const vehicleRoutes = require('./routes/vehicleRoutes');
-
-app.use(express.json()); // for parsing application/json
-app.use('/vehicles', vehicleRoutes); // connecting routes
-
-module.exports = app;
